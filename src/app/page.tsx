9fcf365c228df803f9b1ff757b3de1212525a2e1
Sonupandit9693 <sonupandit969371@gmail.com>
2024-04-25 17:00:53 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
+import { GoogleGeminiEffect } from "@/components/ui/google-gemini-effect";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+      {/* <GoogleGeminiEffect /> */}
+      <Footer />
+    </main>
+  );
+}
